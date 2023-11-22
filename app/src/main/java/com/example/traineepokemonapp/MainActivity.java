61aeee12206fd67e193e84c826f004fd8c5995ae
Pedro Barros <pedrobarros0101@outlook.com>
2023-11-22 16:17:46 -0300

    Intalando e colocando firebase

--- app/src/main/java/com/example/traineepokemonapp/MainActivity.java
+++ app/src/main/java/com/example/traineepokemonapp/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reference.child("permissoes").setValue("all");
     }
 }
