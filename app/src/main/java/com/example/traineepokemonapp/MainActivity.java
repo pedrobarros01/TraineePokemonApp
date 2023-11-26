592fcb71bac9cc4678a7b041bde455a7b988b9ac
Rafael Luna <kenkorio23@gmail.com>
2023-11-26 00:50:27 -0300

    Consertando rota

--- app/src/main/java/com/example/traineepokemonapp/MainActivity.java
+++ app/src/main/java/com/example/traineepokemonapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         textCriarConta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TimeActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CriarContaActivity.class);
                 startActivity(intent);
             }
         });
